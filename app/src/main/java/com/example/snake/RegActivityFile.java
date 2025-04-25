0a3a01f25a5c9a795971663b970589dd04b76761
izaqyos <izaqyos@gmail.com>
2025-04-25 10:35:55 +0300

    starting point. game launches

--- app/src/main/java/com/example/snake/RegActivityFile.java
+++ app/src/main/java/com/example/snake/RegActivityFile.java
@@ -8,11 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegActivityFile extends AppCompatActivity {
 
